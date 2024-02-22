958376fe5ca7612f425729a16d9a1dfa632f1095
Mikhail <futurer@inbox.ru>
2024-02-22 23:58:20 +0300

    Update Main.java

--- src/main/java/com/ecm/pagecounter/Main.java
+++ src/main/java/com/ecm/pagecounter/Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Enter root directory path.");
+        
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
 
